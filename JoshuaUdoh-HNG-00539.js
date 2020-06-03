316db87d8ac560d0b03c885b6e326784f653bb3c
JrideSc <66228055+JrideSc@users.noreply.github.com>
2020-06-03 19:14:58 +0100

    Create JoshuaUdoh-HNG-00539.js

---
+++ JoshuaUdoh-HNG-00539.js
@@ -0,0 +1,9 @@
+{
+    "file": "myScript.py",
+    "output": "Hello World, this is Joshua Udoh with HNGi7 ID HNG-00539 and email jride3space@gmail.com using javascript for stage 2 task",
+    "name": "Joshua Udoh",
+    "id": "HNG-00539",
+    "email": "Jride3space@gmail.com",
+    "language": "Javscript",
+    "status": "Pass"
+  }
